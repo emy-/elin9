79e031b9ae0cb1b96e5b3a48ba3242163a600483
emy- <elin9@u.rochester.edu>
2015-04-10 15:36:12 -0400

    working modal dialog box

--- js/js-whitebored.js
+++ js/js-whitebored.js
@@ -130,3 +130,104 @@ function redraw() {
 		context.stroke();
 	}
 }
+
+/* 
+ * Reference: http://www.paulund.co.uk/how-to-create-a-simple-modal-box-with-jquery 
+ */
+(function($) {
+	//defining jQuery plugin
+	$.fn.modal_box = function(prop) {
+		//defaul parameters
+		var options = $.extend({
+			height: "250",
+			width: "500",
+			title: "Modal Box Demo",
+			description: "good enough for demo?",
+			top: "20%",
+			left: "30%",
+		},prop);
+
+		return this.click(function(e) {
+			add_block_page();
+			add_popup_box();
+			add_styles();
+
+			$('.modal_box').fadeIn();
+		});
+
+		function add_styles() {
+			$('.modal_box').css({ 
+				'position':'absolute', 
+				'left':options.left,
+				'top':options.top,
+				'display':'none',
+				'height': options.height + 'px',
+				'width': options.width + 'px',
+				'border':'1px solid #fff',
+				'box-shadow': '0px 2px 7px #292929',
+				'-moz-box-shadow': '0px 2px 7px #292929',
+				'-webkit-box-shadow': '0px 2px 7px #292929',
+				'border-radius':'10px',
+				'-moz-border-radius':'10px',
+				'-webkit-border-radius':'10px',
+				'background': '#f2f2f2', 
+				'z-index':'50',
+			});
+			$('.p_modal_close').css({
+				'position':'relative',
+				'top':'-25px',
+				'left':'20px',
+				'float':'right',
+				'display':'block',
+				'height':'50px',
+				'width':'50px',
+				'background': 'url(images/close.png) no-repeat',
+			});
+			
+
+			// block page overlay
+			var pageHeight = $(document).height();
+			var pageWidth = $(window).width();
+
+			$('.p_block_page').css({
+				'position': 'absolute',
+				'top': '0',
+				'left': '0',
+				'background-color': 'rgba(0,0,0,0.6)',
+				'height': pageHeight,
+				'width': pageWidth,
+				'z-index': '10'
+			});
+			$('.p_inner_modal_box').css({
+				'background-color':'#fff',
+				'height':(options.height - 50) + 'px',
+				'width':(options.width - 50) + 'px',
+				'padding':'10px',
+				'margin':'15px',
+				'border-radius':'10px',
+				'-moz-border-radius':'10px',
+			        '-webkit-border-radius':'10px'
+			});
+		}
+
+		function add_block_page() {
+			var block_page = $('<div class="p_block_page"></div>');
+
+			$(block_page).appendTo('body');
+		}	
+
+		function add_popup_box() {
+			var pop_up = $('<div class="p_modal_box"><a href="#" class="p_modal_close"></a><div class="p_inner_modal_box"></div></div>');
+			$(pop_up).appendTo('.p_block_page');
+
+			$('.p_modal_close').click(function() {
+				$('.p_block_page').fadeOut().remove();
+				$(this).parent().fadeOut().remove();
+			});
+		}
+
+		return this;
+	};
+})(jQuery);
+
+
